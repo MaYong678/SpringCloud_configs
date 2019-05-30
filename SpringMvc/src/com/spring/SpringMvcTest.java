b7ddd5d4f278460767a2e942a2297609e05d3777
MaYong678 <15388194174@163.com>
2019-05-30 15:49:07 +0800

    2.1

--- SpringMvc/src/com/spring/SpringMvcTest.java
+++ SpringMvc/src/com/spring/SpringMvcTest.java
@@ -2,6 +2,7 @@ package com.spring;
 
 public class SpringMvcTest {
 	public static void main(String[] args) {
-		System.out.println("springmvc");
+		System.out.println("66666666666666");
+		System.out.println("88888888888888");
 	}
 }
