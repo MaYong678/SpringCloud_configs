22dc62c7e4d3329f890fa31f408e89f30588e125
tarena <tarena@tarena-pc>
2019-05-30 14:52:35 +0800

    1.2

--- SpringCloud/src/com/spring/SpringCloudTest.java
+++ SpringCloud/src/com/spring/SpringCloudTest.java
@@ -3,5 +3,6 @@ package com.spring;
 public class SpringCloudTest {
 	public static void main(String[] args) {
 		System.out.println("SpringCloud");
+		System.out.println("SpringBoot");
 	}
 }
---
+++ SpringCloud/src/com/spring/SpringMVCTest.java
@@ -0,0 +1,7 @@
+package com.spring;
+
+public class SpringMVCTest {
+	public static void main(String[] args) {
+		System.out.println("SpringMvc");
+	}
+}
